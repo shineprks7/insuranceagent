5da6e719302df2d2ae6f2bbf633d0e46399a10d9
shineprks7 <prakash.p@hoostack.com>
2025-11-24 10:21:02 +0530

    changes

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -29,3 +29,34 @@ mobilemenu_button.addEventListener('click', function( event ){
 
 
 });
+
+const mobilemenu_link_button = document.querySelector('.mobile-link-button')
+
+mobilemenu_link_button.addEventListener('click', function( event ){
+
+
+   const mobilemenu_button = document.querySelector('.mobile-hamburger-button')
+ 
+ 
+    const mobilemenu_wrapper = document.querySelector('.mobile-menu-wrapper')
+ 
+ 
+   const hasShowmenu =  mobilemenu_wrapper.classList.contains('showmenu');
+ 
+    
+    if(hasShowmenu)
+    {
+      mobilemenu_wrapper.classList.remove('showmenu');
+ 
+      mobilemenu_button.classList.remove('showmenu');
+ 
+    }
+    else
+    {
+      mobilemenu_wrapper.classList.add('showmenu');
+      mobilemenu_button.classList.add('showmenu');
+ 
+    }
+ 
+ 
+ });
